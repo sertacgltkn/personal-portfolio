8237e4ce9f58a022e73a5035084c168c4a1b1f8b
Sertaç Gültekin <sertacgltkn@gmail.com>
2022-10-31 15:28:17 +0300

    projects page added

---
+++ src/pages/projects/ProjectCard.js
@@ -0,0 +1,18 @@
+import { Col } from "react-bootstrap";
+
+export const ProjectCard = ({ title, description, imgUrl }) => {
+  return (
+    <Col size={12} sm={6} md={4}>
+      <div className="project-imgbx">
+        <img
+          style={{ height: "254px", width: "100%", cursor: "pointer" }}
+          src={imgUrl}
+        />
+        <div className="project-text">
+          <h4>{title}</h4>
+          <span>{description}</span>
+        </div>
+      </div>
+    </Col>
+  );
+};
